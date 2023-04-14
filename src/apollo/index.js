e7e36e364165a25695b964d17ee3fd6db54f9b92
Emibrown <emekaekene@gmail.com>
2023-04-14 12:14:50 +0100

    setup .env

--- src/apollo/index.js
+++ src/apollo/index.js
@@ -3,20 +3,14 @@ import {
   InMemoryCache,
   split,
 } from '@apollo/client';
-import merge from 'deepmerge';
-import isEqual from 'lodash-es/isEqual';
 import { createUploadLink } from 'apollo-upload-client';
 import { getMainDefinition } from "@apollo/client/utilities";
 import { GraphQLWsLink } from "@apollo/client/link/subscriptions";
 import { createClient } from "graphql-ws";
   
-export const APOLLO_STATE_PROP_NAME = '__APOLLO_STATE__';
-
-let apolloClient;
-
 // setting configuration for http connect for Query and Mutation
 const httpLink  = createUploadLink({
-    uri:  "http://localhost:8081/graphql",
+    uri:  process.env.NEXT_PUBLIC_HTTP_URL,
     headers: {
       "Apollo-Require-Preflight": true,
     }
@@ -24,15 +18,15 @@ const httpLink  = createUploadLink({
 
 // setting configuration for websocket connect for subscription
 const wsLink =
-    typeof window !== "undefined"
-        ? new GraphQLWsLink(
-                createClient({
-                    url: "ws://localhost:8081/graphql",
-                })
-          )
-        : null;
+  typeof window !== "undefined"
+      ? new GraphQLWsLink(
+              createClient({
+                  url: process.env.NEXT_PUBLIC_WS_URL
+              })
+        )
+      : null;
 
-  const splitLink = typeof window !== "undefined" && wsLink != null
+const splitLink = typeof window !== "undefined" && wsLink != null
   ? split(
           ({ query }) => {
               const def = getMainDefinition(query);
